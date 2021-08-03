199d3759f94eeaa77a0f3992b16031b38deaa145
launchpad5682 <launchpad5682@gmail.com>
2021-08-03 19:37:03 +0530

    header implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import GridImages from "./components/Grid";
+import Header from "./components/Header";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <GridImages />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,29 @@
 import React from "react";
+import "../styles/components/Header.css";
+import windBnBIcon from "../assets/logo.svg";
 
 function Header() {
-  return <div></div>;
+  return (
+    <header className="header-class">
+      <img src={windBnBIcon} className="svg-logo" alt="windBnB Logo" />
+      <form className="form-class">
+        {" "}
+        <input
+          type="text"
+          className="search-input"
+          id="left-input"
+          placeholder="Helsinki, Finland"
+        />
+        <input
+          type="text"
+          className="search-input"
+          id="middle-input"
+          placeholder="Add Guests"
+        />
+        <button className="search-input" id="right-button"></button>
+      </form>
+    </header>
+  );
 }
 
 export default Header;

3c19e418a510a5cff33260e5c74d81cb2050f339
launchpad5682 <launchpad5682@gmail.com>
2021-08-03 23:41:41 +0530

    Grid edited

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,27 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Data from "../assets/stays.json";
 import "../styles/components/Grid.css";
 
 function GridImages() {
   let superHost = false;
+  const [city, setCity] = useState("Vaasa");
 
   return (
     <div className="grid-area">
-      {Data.map((data) => (
-        <div className="image-box">
-          <img src={data.photo} />
-          <div className="image-footer">
-            {data.superHost ? (
-              <span className="superhost-span">SUPER HOST</span>
-            ) : null}
-            <span>
-              {data.type}, {data.beds} beds
-            </span>
-            <span className="rating-span">{data.rating}</span>
-            <p className="title-para">{data.title}</p>
-          </div>
-        </div>
-      ))}
+      {Data.map((data) => {
+        if (city === data.city) {
+          return (
+            <div className="image-box">
+              <img src={data.photo} />
+              <div className="image-footer">
+                {data.superHost ? (
+                  <span className="superhost-span">SUPER HOST</span>
+                ) : null}
+                <span>
+                  {data.type}, {data.beds} beds
+                </span>
+                <span className="rating-span">{data.rating}</span>
+                <p className="title-para">{data.title}</p>
+              </div>
+            </div>
+          );
+        }
+      })}
     </div>
   );
 }

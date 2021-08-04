1a0192331bd1f16fa986ee32593dee642ce36e9d
launchpad5682 <launchpad5682@gmail.com>
2021-08-04 22:46:22 +0530

    Layout and state updated for the cities

--- src/components/SlidingHeader.js
+++ src/components/SlidingHeader.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "../styles/components/SlidingHeader.css";
 import Counter from "./Counter";
+import { PlacesContext } from "../context/PlacesContext";
 
 function SlidingHeader() {
   const cityList = ["Helsinki", "Turku", "Vaasa", "Oulu"];
+  const { selectedCity, setSelectedCity } = useContext(PlacesContext);
+  const [cities, setCities] = useState(false);
+  const [counters, setCounters] = useState(false);
+
+  function renderCitiesList() {
+    setCities(true);
+    setCounters(false);
+  }
+
+  function renderCounters() {
+    setCounters(true);
+    setCities(false);
+  }
+
+  function chooseCity(event) {
+    console.log(event.target.textContent);
+    setSelectedCity(event.target.textContent);
+    setCities(false);
+  }
 
   return (
     <div className="sliding-header">
@@ -13,35 +33,42 @@ function SlidingHeader() {
           type="text"
           className="search-input-header"
           id="left-input-header"
-          placeholder="Helsinki, Finland"
+          placeholder={selectedCity}
+          onClick={renderCitiesList}
         />
         <input
           type="text"
           className="search-input-header"
-          id="middle-input-head  <React.StrictMode>
-          <App />
-        </React.StrictMode>er"
+          id="middle-input-head"
           placeholder="Add Guests"
+          onClick={renderCounters}
         />
         <button className="search-input-header" id="right-button-header">
           Search
         </button>
       </form>
       <div className="drop-downs">
-        <div className="drop-down-div">
-          {cityList.map((city) => {
-            return (
-              <div className="city-list">
-                <img src="https://img.icons8.com/pastel-glyph/32/000000/shipping-location--v1.png" />
-                <p>{city}</p>
-              </div>
-            );
-          })}
-        </div>
-        <div className="drop-down-div">
-          <Counter heading="Adults" subHeading="Ages 13 or above" />
-          <Counter heading="Children" subHeading="Ages 2-12" />
-        </div>
+        {cities ? (
+          <div className="drop-down-div left-down-div">
+            {cityList.map((city) => {
+              return (
+                <div className="city-list" onClick={chooseCity} key={city}>
+                  <img
+                    src="https://img.icons8.com/pastel-glyph/32/000000/shipping-location--v1.png"
+                    alt="location icon"
+                  />
+                  <p>{city}</p>
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
+        {counters ? (
+          <div className="drop-down-div right-down-div">
+            <Counter heading="Adults" subHeading="Ages 13 or above" />
+            <Counter heading="Children" subHeading="Ages 2-12" />
+          </div>
+        ) : null}
       </div>
     </div>
   );
@@ -55,4 +82,6 @@ export default SlidingHeader;
                 <p>{city}</p>
               </div>
             );
-          })}*/
+})}*/
+
+/* */

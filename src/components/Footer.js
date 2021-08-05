c97d4bf72075d7373ab5ea8c9c1da7ac0eb1d385
launchpad5682 <launchpad5682@gmail.com>
2021-08-05 11:55:11 +0530

    Responsive grid implemented

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,14 @@ import "../styles/components/Footer.css";
 function Footer() {
   return (
     <footer>
-      created by <a href="https://github.com/Launchpad5682">launchpad5682</a>
+      created by{" "}
+      <a
+        href="https://github.com/Launchpad5682"
+        target="_blank"
+        rel="noreferrer"
+      >
+        launchpad5682
+      </a>
       -devChallenges.io
     </footer>
   );
